2ada23b019e6f452485af5f4d5c143650bbe3931
Haseeb Ahmed <haseebahmed45@outlook.com>
2024-07-08 17:16:20 +0100

    test: add test for height or weight values set to negative

--- tests/script.test.js
+++ tests/script.test.js
@@ -1,9 +1,10 @@
 const { calculateBMI, getBMICategory } = require('../script.js');
 
-describe('BMI Calculator', () => {
+describe('calculateBMI function', () => {
     test('calculates BMI correctly for valid inputs', () => {
         expect(calculateBMI(180, 75)).toBeCloseTo(23.15, 2);
     });
+
     test('throws an error if height or weight is missing ', () => {
         expect(() => calculateBMI(null, 75)).toThrow('Please enter values for height and weight.');
         expect(() => calculateBMI(180, null)).toThrow('Please enter values for height and weight.');
@@ -12,6 +13,11 @@ describe('BMI Calculator', () => {
     test('throws an error if height or weight is set to 0 ', () => {
         expect(() => calculateBMI(0, 75)).toThrow('Please enter values for height and weight.');
         expect(() => calculateBMI(180, 0)).toThrow('Please enter values for height and weight.');
+    });
+
+    test('throws an error if height or weight is negative', () => {
+        expect(() => calculateBMI(-180, 75)).toThrow('Height and weight must be greater than 0.');
+        expect(() => calculateBMI(180, -75)).toThrow('Height and weight must be greater than 0.');
     });
 });
 
